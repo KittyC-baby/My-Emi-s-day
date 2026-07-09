e21fa0ab65abf56d5df9245998bd6594ea95d066
KittyC-baby <dkdk15042315@gmail.com>
2026-07-09 17:12:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function goNext() {
+    document.body.style.transition = "0.8s";
+    document.body.style.opacity = "0";
 
+    setTimeout(function () {
+        window.location.href = "page2.html";
+    }, 800);
+}
